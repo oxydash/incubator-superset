06a968e0d1de42e4eec1875678ba519d32960d75
Oxystin <vladimir.gladkov@gmail.com>
2018-04-18 23:44:12 +0000

    [Charts] New color palette and margin fix Cal-heatmap chart

--- superset/assets/visualizations/cal_heatmap.js
+++ superset/assets/visualizations/cal_heatmap.js
@@ -3,7 +3,6 @@ import d3 from 'd3';
 
 // CSS
 require('./cal_heatmap.css');
-require('../node_modules/cal-heatmap/cal-heatmap.css');
 
 const CalHeatMap = require('cal-heatmap');
 
@@ -16,11 +15,14 @@ function calHeatmap(slice, payload) {
 
   const timestamps = data.timestamps;
   const extents = d3.extent(Object.keys(timestamps), key => timestamps[key]);
-  const step = (extents[1] - extents[0]) / 5;
+  const step = (extents[1] - extents[0]) / 7;
 
   try {
     cal.init({
       start: data.start,
+      cellSize: 13, 
+      domainMargin: 13,
+      legendMargin: [0,0,0,0],
       data: timestamps,
       itemSelector: slice.selector,
       tooltip: true,
@@ -28,11 +30,11 @@ function calHeatmap(slice, payload) {
       subDomain: data.subdomain,
       range: data.range,
       browsing: true,
-      legend: [extents[0], extents[0] + step, extents[0] + (step * 2), extents[0] + (step * 3)],
+      legend: [extents[0], extents[0] + step, extents[0] + (step * 2), extents[0] + (step * 3), extents[0] + (step * 4), extents[0] + (step * 5)],
     });
   } catch (e) {
     slice.error(e);
   }
-}
+} 
 
 module.exports = calHeatmap;
